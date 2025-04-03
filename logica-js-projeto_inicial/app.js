3a994b8fb67ee49fd5dd52a9611f0ea071d26b80
Renan Siqueira dos Passos <rsiqueira78@gmail.com>
2025-04-03 08:34:56 -0300

    commit desafios logica js
    Commit dos desafios do JS feito no módulo 2 do curso na Alura

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,49 @@
+//Exercício 1
+
+// let diaSemana = prompt('Que dia da semana é hoje?');
+
+// if(diaSemana == 'Sábado' || diaSemana == 'sabado' || diaSemana == 'Domingo'|| diaSemana == 'domingo'){
+//     window.alert('Bom final de semana!');
+// }else{
+//     window.alert('Boa semana!');
+// }
+
+
+//Exercício 2
+
+// let numero = prompt('Digite um número');
+
+// if(numero >= 0){
+//     window.alert('O número é positivo');
+// }else{
+//     window.alert('o número é negativo');
+// }
+
+
+//Exercício 3
+
+// let pontos = prompt('Qual a pontuação?');
+
+// if(pontos >= 100){
+//     window.alert('Parabéns, você venceu!');
+// }else{
+//     window.alert('Tente novamente para ganhar.');
+// }
+
+
+//Exercício 4
+
+// let saldo = 100;
+
+// let saque = prompt('Qual valor deseja sacar?');
+
+// while(saque > saldo){
+//     alert(`O valor de saque é superior ao valor em conta, insira um novo valor.`)
+//     saque = prompt('Qual valor deseja sacar?');
+// }
+//     saldo = saldo - saque;
+//     alert(`Saldo atualizado, agora você tem R$${saldo}`);
+
+//Exercício 5
+let nome = prompt('Insira seu nome');
+alert(`Seja bem-vindo ao sistema, ${nome}!`);

ecdeb502522df7605665b2d0070b98011674fff8
Renan Siqueira dos Passos <rsiqueira78@gmail.com>
2025-04-03 09:21:19 -0300

    commit desafios
    resolução de desafios do módulo 3 de lógica JS - Alura

---
+++ desafios/desafio02/app.js
@@ -0,0 +1,45 @@
+// Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+// Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+
+// //Exercício 1
+
+// let contador = 1;
+// while(contador <= 10){
+//     console.log(contador);
+//     contador++;
+// }
+
+
+// //Exercício 2
+
+// let contador = 10;
+// while(contador >= 0){
+//     alert(contador);
+//     contador--;
+// }
+
+
+// //Exercício 3
+
+// let num = window.prompt('Escolha um número')
+
+// while(num >= 0){
+//     console.log(num);
+//     num--;
+// }
+
+//Exercício 4
+
+let num = window.prompt('Escolha um número');
+let contador = 0;
+
+while(contador <= num){
+    console.log(contador);
+    contador++;
+}
